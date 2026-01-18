96919f5a80985633569ee329f1a724214382719c
SirCaio55 <caioluizgsa68@gmail.com>
2026-01-18 18:42:08 -0300

    Conversor de moedas
    Desenvolvimento de uma aplicação em Python para conversão de valores entre diferentes moedas.

---
+++ PegarCotaçao.py
@@ -0,0 +1,11 @@
+import requests
+
+def pegar_cotaçao_moeda(moeda_origem, moeda_destino):
+    link = f'https://economia.awesomeapi.com.br/last/{moeda_origem}-{moeda_destino}'
+    requisiçao = requests.get(link)
+    
+    cotaçao = requisiçao.json()[f'{moeda_origem}{moeda_destino}']['bid']
+    return cotaçao
+    
+    
+
